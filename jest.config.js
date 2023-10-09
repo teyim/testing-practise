f8ffd7144bb6708c6b227983905d22dc9e1c26ba
teyim <t.muluh@batazia.com>
2023-10-09 13:18:41 +0100

    modify jest config

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.jest.json" }],
   },
   setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
